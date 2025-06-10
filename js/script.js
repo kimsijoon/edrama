d8c6c54bc64a87504ca861bace36d7cec11486ca
Sijoon Kim <42532369+kimsijoon@users.noreply.github.com>
2025-06-10 17:55:00 +0900

    Adjust css

--- js/script.js
+++ js/script.js
@@ -7,19 +7,26 @@ window.onload = async function () {
   nextQuestion();
 };
 
-function nextQuestion() {
-  document.getElementById("result").innerText = "";
-  document.getElementById("answer").value = "";
-  current = data[Math.floor(Math.random() * data.length)];
-  document.getElementById("question").innerText = `"${current.kr}" 영어로?`;
-}
-
-function checkAnswer() {
-  const userAnswer = document.getElementById("answer").value.trim().toLowerCase();
-  const correct = current.en.toLowerCase();
-  if (userAnswer === correct) {
-    document.getElementById("result").innerText = "✅ 정답입니다!";
-  } else {
-    document.getElementById("result").innerText = `❌ 오답입니다. 정답: ${current.en}`;
+document.getElementById("answer").addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault(); 
+    document.getElementById("answer").innerText = "Enter!";
   }
-}
+});
+
+// function nextQuestion() {
+//   document.getElementById("result").innerText = "";
+//   document.getElementById("answer").value = "";
+//   current = data[Math.floor(Math.random() * data.length)];
+//   document.getElementById("question").innerText = `"${current.kr}" 영어로?`;
+// }
+
+// function checkAnswer() {
+//   const userAnswer = document.getElementById("answer").value.trim().toLowerCase();
+//   const correct = current.en.toLowerCase();
+//   if (userAnswer === correct) {
+//     document.getElementById("result").innerText = "✅ 정답입니다!";
+//   } else {
+//     document.getElementById("result").innerText = `❌ 오답입니다. 정답: ${current.en}`;
+//   }
+// }
